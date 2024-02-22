a48e8126878be4cf8b4bea330bb3496255458f80
papruz.289857 <papruz.289857@LAB-A-PC13>
2024-02-23 07:51:51 +0800

    Add project files.

---
+++ ComputerAverageApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ComputerAverageApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.WriteLine("Enter 5 grades separated by new line: ");
+            double add = 0;
+
+                for (int i = 0; i < 5; i++) 
+                {
+                double grades = Convert.ToDouble(Console.ReadLine());
+                add += grades;
+                }
+
+                double avg = add / 5;
+                double roundAvg = Math.Round(avg);
+                Console.WriteLine("The average is " + avg + " and round off to " + roundAvg );
+               
+            
+        }
+    }
+}
